f4efd5c00824ccf45f73bac8f978fb7ed3f651c9
Abhay Pokharkar <abhaypokharkar@gmail.com>
2026-01-31 14:27:14 +0530

    Refactor BackgroundSmsPlugin for SMS functionality
    Refactor BackgroundSmsPlugin for improved SMS handling and code clarity.

--- android/src/main/java/com/j/background_sms/BackgroundSmsPlugin.java
+++ android/src/main/java/com/j/background_sms/BackgroundSmsPlugin.java
@@ -1,49 +1,27 @@
 package com.j.background_sms;
 
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.os.Build;
 import android.telephony.SmsManager;
 
 import androidx.annotation.NonNull;
 
-import java.util.UUID;
+import java.util.ArrayList;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-import java.util.ArrayList;
 
 /** BackgroundSmsPlugin */
 public class BackgroundSmsPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+
   private MethodChannel channel;
 
   @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "background_sms");
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), "background_sms");
     channel.setMethodCallHandler(this);
-  }
-
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "background_sms");
-    channel.setMethodCallHandler(new BackgroundSmsPlugin());
   }
 
   @Override
@@ -53,48 +31,42 @@ public class BackgroundSmsPlugin implements FlutterPlugin, MethodCallHandler {
       String msg = call.argument("msg");
       Integer simSlot = call.argument("simSlot");
       sendSMS(num, msg, simSlot, result);
-    }else if(call.method.equals("isSupportMultiSim")) {
+    } else if (call.method.equals("isSupportMultiSim")) {
       isSupportCustomSim(result);
-    } else{
+    } else {
       result.notImplemented();
     }
   }
 
-  private void isSupportCustomSim(Result result){
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-      result.success(true);
-    }else{
-      result.success(false);
-    }
+  private void isSupportCustomSim(Result result) {
+    result.success(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O);
   }
 
-  private void sendSMS(String num, String msg, Integer simSlot,Result result) {
+  private void sendSMS(String num, String msg, Integer simSlot, Result result) {
     try {
       SmsManager smsManager;
-      if (simSlot == null) {
-        smsManager = SmsManager.getDefault();
+
+      if (simSlot != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        smsManager = SmsManager.getSmsManagerForSubscriptionId(simSlot);
       } else {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          smsManager = SmsManager.getSmsManagerForSubscriptionId(simSlot);
-        } else {
-          smsManager = SmsManager.getDefault();
-        }
+        smsManager = SmsManager.getDefault();
       }
-      if(msg.length() < 120){
+
+      if (msg.length() < 120) {
         smsManager.sendTextMessage(num, null, msg, null, null);
-      } else{
+      } else {
         int size = 120;
-        ArrayList<String> split = new ArrayList<>();
+        ArrayList<String> parts = new ArrayList<>();
         for (int i = 0; i <= msg.length() / size; i++) {
-            split.add(msg.substring(i * size, Math.min((i + 1) * size, msg.length())));
+          parts.add(msg.substring(i * size, Math.min((i + 1) * size, msg.length())));
         }
-        // System.out.println(split);
-        smsManager.sendMultipartTextMessage(num, null, split, null, null);
+        smsManager.sendMultipartTextMessage(num, null, parts, null, null);
       }
+
       result.success("Sent");
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      result.error("Failed", "Sms Not Sent", "");
+    } catch (Exception e) {
+      e.printStackTrace();
+      result.error("FAILED", "Sms not sent", null);
     }
   }
 
